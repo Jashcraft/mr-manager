9ce094d4eafc6e964363bcfad1fb006676bcf73c
Jashcraft <ashcraft.jonathon@gmail.com>
2022-05-12 19:51:25 -0600

    add initial prompt

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
+const initialQuestions = [
+  {
+    message: "Would you like to add a team member?",
+    type: "confirm",
+    name: "addMember"
+  }
+]
+
+const startApp = function(){
+  inquirer.prompt(initialQuestions)
+} 
+
+startApp();
