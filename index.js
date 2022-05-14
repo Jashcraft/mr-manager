af8866d5e46ebb755ebaf649ffb076a623c5f13d
Jashcraft <ashcraft.jonathon@gmail.com>
2022-05-14 15:47:58 -0600

    add individual team add questions + log

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
+const teamMembers = [];
+
 const initialQuestions = [
   {
     message: "Would you like to add a team member?",
@@ -13,8 +15,67 @@ const initialQuestions = [
   }
 ]
 
+const addTeamMember = () => {
+  const memberTypeQuestion = [
+    {
+      message: "What type of team member would you like to create",
+      type: "list",
+      name: "memberType",
+      choices: [
+        "Manager",
+        "Engineer",
+        "Intern"
+      ]
+    }
+  ];
+  inquirer.prompt(memberTypeQuestion)
+  .then(answers => {
+    switch (answers.memberType) {
+      case "Manager":
+        createManager();
+        break;
+      case "Engineer":
+        createEngineer();
+      case "Intern":
+        createIntern();
+      default:
+        break;
+    }
+  })
+}
+
+
+const createManager = () => {
+  //this will create a manager by asking manager related prompts
+  console.log('running the createManager function') 
+}
+
+const createEngineer = () => {
+  //this will create an Engineer by asking Engineer related prompts
+  console.log('running the createEngineer function') 
+}
+
+const createIntern = () => {
+  // this will create an intern by asking Intern related prompts
+  console.log('running the createIntern function')
+}
+
+
 const startApp = function(){
   inquirer.prompt(initialQuestions)
+  .then(answers => {
+    if(answers.addMember){
+      addTeamMember();
+    }
+    else {
+      if(!teamMembers.length){
+        console.log("At least one team member required. Please create a team member.")
+        startApp();
+      } else {
+        buildTeam();
+      }
+    }
+  })
 } 
 
 startApp();
